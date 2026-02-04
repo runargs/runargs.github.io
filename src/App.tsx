e97ea6d8bf831a561a2f43b8f366e6733f3403fc
Alexa Thoennes <alnbaldon@gmail.com>
2026-02-03 23:33:38 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import NotFound from "./pages/ResumeOnePager";
+import ResumeOnePager from "./pages/ResumeOnePager";
 
 const queryClient = new QueryClient();
 
@@ -17,8 +17,11 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          
+          {/* Your custom PM Resume Route */}
           <Route path="/cv" element={<ResumeOnePager />} />
+          
+          {/* Catch-all route must be last */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
