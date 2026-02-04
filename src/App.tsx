d6254a30f13982d1f8155007b6c498fee05bded7
Alexa Thoennes <alnbaldon@gmail.com>
2026-02-03 23:22:28 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/cv" element={<ResumeOnePager />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
